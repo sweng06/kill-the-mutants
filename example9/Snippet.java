2d58ae96e7ff28ab0b1e4a7d057b0bd1cfebcd04
Swana Weng <sweng@umass.edu>
2015-12-03 14:02:38 -0500

    pushed example 7,8,9 since example6 worked with docker

---
+++ example9/Snippet.java
@@ -0,0 +1,23 @@
+package example9;
+
+import java.util.ArrayList;
+
+public class Snippet {
+	//array of [num] numbers. if number is odd, insert into arraylist + 1
+	public ArrayList<Integer> modArray(){
+		int num =+ 10;
+		int[] array = new int[num];
+		ArrayList<Integer> newArray = new ArrayList<Integer>();
+		
+		for(int i = 0; i<array.length;i++){
+			array[i] = i;
+		}
+		
+		for(int i = 0; i<array.length;i++){
+			if(array[i] % 2 > 0){
+				newArray.add(array[i] + 1);
+			}
+		}
+		return newArray;
+	}
+}
