7b25519c9075d64b522c886b8f6b856a2c9ec322
Swana Weng <sweng@umass.edu>
2015-12-03 11:52:40 -0500

    added example6 to test on docker

---
+++ example6/0000000006/Tests.java
@@ -0,0 +1,22 @@
+package example6;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Tests {
+
+	private Snippet h;
+
+	@Before
+	public void setUp() throws Exception {
+		h = new Snippet();
+	}
+
+	@Test
+	public void testAddNums() {
+		assertEquals(h.addNums(1, 2), 3);
+	}
+
+}
