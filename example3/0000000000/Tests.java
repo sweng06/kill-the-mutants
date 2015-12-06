bcf44918b91505de1b5eb602300afe4fbadd7dee
Ryan Connell <RConnell425@gmail.com>
2015-12-06 16:18:12 -0500

    Trying to kill more mutants

--- example3/0000000000/Tests.java
+++ example3/0000000000/Tests.java
@@ -27,7 +27,8 @@ public class Tests
 	public void test() {
 		int[] array = {2,4,5,3,1,7,4,9,10};
 		int[] sortedArray = h.doInsertionSort(array);
-		assertArrayEquals(sortedArray, new int[]{1,2,3,4,4,5,12,9,10});
+		assertEquals(array.length, sortedArray.length);
+		assertArrayEquals(sortedArray, new int[]{1,2,3,4,4,5,7,9,10});
 	}
 
 }
