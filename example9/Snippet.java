53a9fa6a0e3c380f5afa985849de40154c81c835
Swana Weng <sweng@umass.edu>
2015-12-06 15:55:40 -0500

    no message

--- example9/Snippet.java
+++ example9/Snippet.java
@@ -3,19 +3,15 @@ package example9;
 import java.util.ArrayList;
 
 public class Snippet {
-	//array of [num] numbers. if number is odd, insert into arraylist + 1
-	public ArrayList<Integer> modArray(){
-		int num =+ 10;
-		int[] array = new int[num];
+
+	public ArrayList<Integer> modArray() {
+		int[] array = new int[20];
 		ArrayList<Integer> newArray = new ArrayList<Integer>();
-		
-		for(int i = 0; i<array.length;i++){
+
+		for (int i = 0; i < array.length; i++) {
 			array[i] = i;
-		}
-		
-		for(int i = 0; i<array.length;i++){
-			if(array[i] % 2 > 0){
-				newArray.add(array[i] + 1);
+			if (array[i] < 10) {
+				newArray.add(array[i]);
 			}
 		}
 		return newArray;
