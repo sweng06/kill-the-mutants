29d39973bf30ff6d5bca14055e2f85d8f26c50da
Swana Weng <sweng@umass.edu>
2015-12-06 21:19:28 -0500

    no message

--- example8/0000000000/Tests.java
+++ example8/0000000000/Tests.java
@@ -15,17 +15,10 @@ public class Tests {
 	}
 
 	@Test
-	public void testFirstCase() {
+	public void test() {
 		assertEquals(h.printWord(0, 1), "Mutation Testing");
-	}
-
-	@Test
-	public void testSecondCase() {
 		assertEquals(h.printWord(1, 0), "M");
-	}
-
-	@Test
-	public void testThirdCase() {
 		assertEquals(h.printWord(1, 1), "M");
 	}
+
 }
