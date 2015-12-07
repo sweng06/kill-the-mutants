37ec19815f7cd406f094fa935ca927597401f950
Ryan Connell <RConnell425@gmail.com>
2015-12-06 21:39:46 -0500

    e8

--- example8/0000000000/Tests.java
+++ example8/0000000000/Tests.java
@@ -16,9 +16,9 @@ public class Tests {
 
 	@Test
 	public void test() {
-		assertEquals(h.printWord(0, 1), "Mutation Testing");
-		assertEquals(h.printWord(1, 0), "M");
-		assertEquals(h.printWord(1, 1), "M");
+		assertEquals(h.printWord(0, 1), "mutation");
+		assertEquals(h.printWord(1, 0), "testing");
+		assertEquals(h.printWord(1, 1), "kill-the-mutants");
 	}
 
 }
--- example8/Snippet.java
+++ example8/Snippet.java
@@ -1,18 +1,13 @@
 package example8;
 
 public class Snippet {
-	// Prints out a word if x<y, else, print out one char of word
+	
 	public String printWord(int x, int y) {
-
-		String word = "Mutation Testing";
-		String trueWord;
-
-		if (x < y) {
-			trueWord = word;
-		} else {
-			char wordChar = word.charAt(0);
-			trueWord = String.valueOf(wordChar);
-		}
-		return trueWord;
+		if (x < y)
+			return "mutation";
+		if (x > y)
+			return "testing";
+		
+		return "kill-the-mutants";
 	}
 }
