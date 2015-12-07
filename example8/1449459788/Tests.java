dad093eff9c535902514685b0ae3ea322afe842d
Swana <sweng@umass.edu>
2015-12-07 03:43:17 +0000

    or... whatever

---
+++ example8/1449459788/Tests.java
@@ -0,0 +1,24 @@
+package example8;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Tests {
+
+	private Snippet h;
+
+	@Before
+	public void setUp() throws Exception {
+		h = new Snippet();
+	}
+
+	@Test
+	public void test() {
+		assertEquals(h.printWord(0, 1), "mutation");
+		assertEquals(h.printWord(1, 0), "testing");
+	//	assertEquals(h.printWord(1, 1), "kill-the-mutants");
+	}
+
+}
